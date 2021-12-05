cdb3129260c212a1774937273016016192c97dcc
Scott Nason <scottmnason@gmail.com>
2021-12-05 14:37:06 -0700

    commit tree 72d9a2f19afb6548eeb6076fe6b82d98a09f64f0

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup){
+    return Math.abs(pickup - 42);
+}
+// Math.abs from a Google search, which brought me to StackOverflow
+
+function distanceFromHqInFeet(pickup){
+    return distanceFromHqInBlocks(pickup) * 264;
+}
+
+function distanceTravelledInFeet(start, end){
+    return Math.abs(start - end) * 264;
+}
+
+function calculatesFarePrice(start, end){
+    if (distanceTravelledInFeet(start, end) <= 400){
+        return 0;
+    } else if (distanceTravelledInFeet(start, end) <= 2000) {
+        return (distanceTravelledInFeet(start, end) - 400) * 0.02; 
+    } else if (distanceTravelledInFeet(start, end) > 2500) {
+        return 'cannot travel that far';
+    } else
+        return 25;
+}
